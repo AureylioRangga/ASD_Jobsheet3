53bfff1f2d096f07618e4132a529217620065841
Aureylio <aureyliorangga2@gmail.com>
2026-02-24 14:53:19 +0700

    Tugas No 2 (DosenDemo07)

--- DosenDemo07.java
+++ DosenDemo07.java
@@ -4,15 +4,16 @@ public class DosenDemo07 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
 
         System.out.print("Masukkan jumlah dosen: ");
-        int jumlah = input.nextInt();
+        int jumlah = input.nextInt();   
         input.nextLine();
 
+        // membuat array of object Dosen sebanyak jumlah yang diinput
         Dosen07[] arrayOfDosen = new Dosen07[jumlah];
 
-        // input FOR
+        // looping FOR untuk input data dosen
         for (int i = 0; i < jumlah; i++) {
 
             System.out.println("Masukkan Data Dosen ke-" + (i + 1));
@@ -24,41 +25,49 @@ public class DosenDemo07 {
             String nama = input.nextLine();
 
             System.out.print("Jenis Kelamin  : ");
-            Boolean jk = input.nextBoolean();
+            String jkInput = input.nextLine();
 
             System.out.print("Usia           : ");
             int usia = input.nextInt();
-            input.nextLine();
+            input.nextLine(); 
 
+            // membuat variabel Boolean untuk jenis kelamin
+            Boolean jk;
+
+            if (jkInput.equalsIgnoreCase("Laki-laki")) {
+
+                jk = true; 
+
+            } else {
+
+                jk = false;
+
+            }
+
+            // membuat object Dosen dan menyimpannya ke array
             arrayOfDosen[i] = new Dosen07(kode, nama, jk, usia);
 
-            System.out.println("----------------------");
+            System.out.println("-------------------------------");
 
         }
 
-        // tampil FOREACH
         System.out.println("\nDATA DOSEN");
 
-        int no = 1;
+        int no = 1; // nomor urutan dosen
 
+        // looping ForEach untuk menampilkan data
         for (Dosen07 d : arrayOfDosen) {
 
             System.out.println("Data Dosen ke-" + no++);
-            System.out.println("Kode            : " + d.kode);
-            System.out.println("Nama            : " + d.nama);
-            System.out.println("Jenis Kelamin   : " + (d.jenisKelamin ? "Pria" : "Wanita"));
-            System.out.println("Usia            : " + d.usia);
-            System.out.println("------------------------");
+
+            System.out.println("Kode           : " + d.kode);
+            System.out.println("Nama           : " + d.nama);
+            System.out.println("Jenis Kelamin  : " + (d.jenisKelamin ? "Laki-laki" : "Perempuan"));
+            System.out.println("Usia           : " + d.usia);
+
+            System.out.println("--------------------------------");
 
         }
-
-        // panggil class DataDosen
-        DataDosen07 data = new DataDosen07();
-
-        data.jumlahDosenPerJenisKelamin(arrayOfDosen);
-        data.rerataUsiaDosenPerJenisKelamin(arrayOfDosen);
-        data.infoDosenPalingTua(arrayOfDosen);
-        data.infoDosenPalingMuda(arrayOfDosen);
 
     }
 
