3e8760708ac8aa0f58d2308714f5e4d31043470e
Aureylio <aureyliorangga2@gmail.com>
2026-02-24 11:11:08 +0700

    Percobaan 3 MataKuliah

---
+++ MataKuliahDemo07.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class MataKuliahDemo07 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        MataKuliah07[] arrayMataKuliah = new MataKuliah07[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i=0; i<3; i++){
+            System.out.println("Masukkan Data Mata Kuliah Ke-" + (i+1) + " : ");
+            System.out.print("Kode       :  ");
+            kode = sc.nextLine();
+            System.out.print("Nama       :  ");
+            nama = sc.nextLine();
+            System.out.print("SKS        :  ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam :  ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("-------------------------------------");  
+
+            arrayMataKuliah[i] = new MataKuliah07(kode, nama, sks, jumlahJam);
+        }
+
+        for (int j=0; j<3; j++){
+            System.out.println("Data Mata Kuliah Ke-" + (j+1) + " : ");
+            System.out.println("Kode       : " + arrayMataKuliah[j].kode);
+            System.out.println("Nama       : " + arrayMataKuliah[j].nama);
+            System.out.println("SKS        : " + arrayMataKuliah[j].sks);
+            System.out.println("Jumlah Jam : " + arrayMataKuliah[j].jumlahJam);
+            System.out.println("-------------------------------------");  
+        }
+    }
+}
