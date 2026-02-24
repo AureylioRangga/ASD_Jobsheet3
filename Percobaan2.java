11984b26c1842f7315ca113c607269b033967757
Aureylio <aureyliorangga2@gmail.com>
2026-02-24 10:18:42 +0700

    Percobaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,6 +1,27 @@
-public class MahasiswaDemo07 {
+import java.util.Scanner;
+public class Percobaan2 {
     public static void main(String[]args) {
+        
         Mahasiswa07[] arrayOfMahasiswa = new Mahasiswa07[3];
+
+        Scanner sc = new Scanner(System.in);
+        String dummy;
+
+        for(int i=0 ; i<3; i++){
+            arrayOfMahasiswa[i] = new Mahasiswa07();
+
+            System.out.println("Masukkan Data Mahasiswa ke- "+(i+1)+" : ");
+            System.out.print("NIM   : ");
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("-------------------------------------");
+        }
 
         arrayOfMahasiswa[0] = new Mahasiswa07();
         arrayOfMahasiswa[0]. nim = "2441070600033";
@@ -19,6 +40,16 @@ public class MahasiswaDemo07 {
         arrayOfMahasiswa[2]. nama = "DHIRHAMAWAN PUTRANTO";
         arrayOfMahasiswa[2]. kelas = "TI - 2A";
         arrayOfMahasiswa[2]. ipk = (float)3.8;
+        
+
+        for (int i=0; i<3; i++){
+            System.out.println("Data Mahasiswa ke-" + (i+1));
+            System.out.println("NIM     : "+ arrayOfMahasiswa[i].nim);
+            System.out.println("Nama    : "+ arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas   : "+ arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK     : "+ arrayOfMahasiswa[i].ipk);
+            System.out.println("-------------------------------------");
+        }
 
         System.out.println("NIM      : "+ arrayOfMahasiswa[0].nim);
         System.out.println("Nama     : "+ arrayOfMahasiswa[0].nama);
