dc6d1a77805546d924b8944c062b63ee834651ab
Aureylio <aureyliorangga2@gmail.com>
2026-02-24 10:34:07 +0700

    Percobaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -12,10 +12,13 @@ public class Percobaan2 {
 
             System.out.println("Masukkan Data Mahasiswa ke- "+(i+1)+" : ");
             System.out.print("NIM   : ");
+
             arrayOfMahasiswa[i].nim = sc.nextLine();
             System.out.print("Nama  : ");
+
             arrayOfMahasiswa[i].nama = sc.nextLine();
             System.out.print("Kelas : ");
+
             arrayOfMahasiswa[i].kelas = sc.nextLine();
             System.out.print("IPK   : ");
             dummy = sc.nextLine();
@@ -44,26 +47,12 @@ public class Percobaan2 {
 
         for (int i=0; i<3; i++){
             System.out.println("Data Mahasiswa ke-" + (i+1));
+            
             System.out.println("NIM     : "+ arrayOfMahasiswa[i].nim);
             System.out.println("Nama    : "+ arrayOfMahasiswa[i].nama);
             System.out.println("Kelas   : "+ arrayOfMahasiswa[i].kelas);
             System.out.println("IPK     : "+ arrayOfMahasiswa[i].ipk);
             System.out.println("-------------------------------------");
         }
-
-        System.out.println("NIM      : "+ arrayOfMahasiswa[0].nim);
-        System.out.println("Nama     : "+ arrayOfMahasiswa[0].nama);
-        System.out.println("Kelas    : "+ arrayOfMahasiswa[0].kelas);
-        System.out.println("IPK      : "+ arrayOfMahasiswa[0].ipk);
-        System.out.println("-------------------------------------");
-        System.out.println("NIM      : "+ arrayOfMahasiswa[1].nim);
-        System.out.println("Nama     : "+ arrayOfMahasiswa[1].nama);
-        System.out.println("Kelas    : "+ arrayOfMahasiswa[1].kelas);
-        System.out.println("IPK      : "+ arrayOfMahasiswa[1].ipk);
-        System.out.println("-------------------------------------");
-        System.out.println("NIM      : "+ arrayOfMahasiswa[2].nim);
-        System.out.println("Nama     : "+ arrayOfMahasiswa[2].nama);
-        System.out.println("Kelas    : "+ arrayOfMahasiswa[2].kelas);
-        System.out.println("IPK      : "+ arrayOfMahasiswa[2].ipk);
     }
 }
